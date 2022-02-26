96e53e63ef2e2d2c2bdf5e64873c04f31ee33deb
ᴸᵘᶜʳᵉᶜᵉ ˢʰⁱⁿ <lucrece.shin@utoronto.ca>
2022-02-26 01:17:12 -0500

    Fixed minor bugs in the main .py file

--- Skip_Grams_Negative_Sampling.py
+++ Skip_Grams_Negative_Sampling.py
@@ -6,7 +6,7 @@ with open("data/text8.txt") as f:
     
   
 ###############################################################################
-##########################  2. TEXT PRE-PROCESSING  ###########################
+###########################  2. PRE-PROCESS TEXT  #############################
 ###############################################################################
 import utils
 words = utils.preprocess(text)
@@ -20,7 +20,7 @@ int_words = [vocab_to_int[word] for word in words]
 
 
 ###############################################################################
-#############################  4. SUBSAMPLING   ###############################
+#########################  4. PERFORM SUBSAMPLING   ###########################
 ###############################################################################
 from collections import Counter
 import numpy as np
@@ -36,11 +36,13 @@ train_words = [word for word in int_words if p_drop[word] < (1 - random.random()
 ###############################################################################
 ################  5. GET CONTEXT TARGETS FOR EACH WORD INPUT   ################
 ###############################################################################
-# get a random range of 1-5 context word targets for word input with index 'idx' from 'words'
-def get_targets(words, idx, window_size = 5):
+# get a random range of 1-5 context word targets on the left & right of the word input with index 'idx' from 'words'
+def get_targets(words, idx, window_size=5):
     
     R = random.randint(1, 5)
-    targets = words[max(0, idx-R) : idx] + words[idx+1 : min(idx+R+1, len(words))]
+    start = max(0,idx-R)
+    end = min(idx+R,len(words)-1)
+    targets = words[start:idx] + words[idx+1:end+1] # +1 since doesn't include this idx
     
     return targets
 
@@ -64,22 +66,28 @@ def get_batches(words, batch_size, window_size = 5):
         
 
 ###############################################################################
-#################  7. VALIDATION METRIC: COSINE SIMILARITY  ###################
+##############  7. DEFINE VALIDATION METRIC: COSINE SIMILARITY  ###############
 ###############################################################################
 import torch
 
-def cosine_similarity(embedding, valid_size = 16, valid_window = 100, device = 'cpu'):
-    embed_vectors = embedding.weight  # shape: (n_vocab, embed_dim) 
-    magnitudes = embed_vectors.pow(2).sum(dim=1).sqrt().unsqueeze(0)
-    
-    valid_idxs = random.sample(range(valid_window), valid_size/2)
-    valid_idxs = np.append(valid_idxs, 
-                           random.sample(range(1000, 1000+valid_window), valid_size/2))
-    valid_idxs = torch.LongTensor(valid_idxs).to(device) # (16, 1)
-    valid_vectors = embedding(valid_idxs) # (16, embed_dim)
-    similarities = torch.mm(valid_vectors, embed_vectors.t()) / magnitudes  # (16, n_vocab)
-    
-    return valid_idxs, similarities
+def cosine_similarity(embedding, n_valid_words=16, valid_window=100):
+    """ Returns the cosine similarity of validation words with words in the embedding matrix.
+        embedding: PyTorch embedding module
+        n_valid_words: # of validation words (recommended to have even numbers)
+    """
+    all_embeddings = embedding.weight  # (n_vocab, n_embed) 
+    # sim = (a . b) / |a||b|
+    magnitudes = all_embeddings.pow(2).sum(dim=1).sqrt().unsqueeze(0) # (1, n_vocab)
+  
+    # Pick validation words from 2 ranges: (0, window): common words & (1000, 1000+window): uncommon words 
+    valid_words = random.sample(range(valid_window), n_valid_words//2) + random.sample(range(1000, 1000+valid_window), n_valid_words//2)
+    valid_words = torch.LongTensor(np.array(valid_words)).to(device) # (n_valid_words, 1)
+
+    valid_embeddings = embedding(valid_words) # (n_valid_words, n_embed)
+    # (n_valid_words, n_embed) * (n_embed, n_vocab) --> (n_valid_words, n_vocab) / 1, n_vocab)
+    similarities = torch.mm(valid_embeddings, all_embeddings.t()) / magnitudes  # (n_valid_words, n_vocab)
+  
+    return valid_words, similarities
         
 
 ###############################################################################
@@ -88,82 +96,79 @@ def cosine_similarity(embedding, valid_size = 16, valid_window = 100, device = '
 from torch import nn
 from torch.functional import F
 
-class SkipGram_NegSample_Model(nn.module):
-    
-    def __init__(self, n_vocab, n_embed, noise_dist = None):
+class SkipGram_NegSample_Model(nn.Module):
+    def __init__(self, n_vocab, n_embed, noise_dist=None):
         super().__init__()
-        self.n_vocab = n_vocab  # number of UNIQUE words in vocabulary
-        self.n_embed = n_embed  # embedding dimension
-        self.noise_dist = noise_dist  # probability distribution to pick noise samples from vocabulary
         
-        self.input_embed = nn.Embedding(n_vocab, n_embed)
-        self.output_embed = nn.Embeddimg(n_vocab, n_embed)
+        self.n_vocab = n_vocab
+        self.n_embed = n_embed
+        self.noise_dist = noise_dist
         
-        # initialize weights (= embedding tables) --> may help converge faster
-        self.input_embed.weight.uniform_(-1, 1)
-        self.output_embed.weight.uniform(-1, 1)
+        self.in_embed = nn.Embedding(n_vocab, n_embed)
+        self.out_embed = nn.Embedding(n_vocab, n_embed)
         
+        # Initialize both embedding tables with uniform distribution
+        self.in_embed.weight.data.uniform_(-1, 1)
+        self.out_embed.weight.data.uniform_(-1, 1)
         
-    def forward_input (self, input_words):
-        embedded_input_words = self.input_embed(input_words)
-        
-        return embedded_input_words
+
+    def forward_input(self, input_words):
+        input_vectors = self.in_embed(input_words)
+        return input_vectors  # input vector embeddings
     
+
+    def forward_target(self, output_words):
+        output_vectors = self.out_embed(output_words)
+        return output_vectors  # output vector embeddings
     
-    def forward_target (self, target_words):
-        embedded_target_words = self.output_embed(target_words)
-        
-        return embedded_target_words
-    
-    
-    def forward_noise (self, batch_size, n_samples):
+
+    def forward_noise(self, batch_size, n_samples=5):
+        """ Generate noise vectors with shape (batch_size, n_samples, n_embed)"""
         # If no Noise Distribution specified, sample noise words uniformly from vocabulary
         if self.noise_dist is None:
-            noise_dist = torch.ones(self.n_vocab)  # shape : [n_vocab]
+            noise_dist = torch.ones(self.n_vocab)
         else:
-            noise_dist = self.noise_dist   # shape : [n_vocab]
+            noise_dist = self.noise_dist
             
-        noise_words = torch.multinomial(noise_dist, batch_size * n_samples, replacement = True)
-        device = 'cuda' if torch.cuda.is_available else 'cpu'
+        # torch.multinomial :
+        # Returns a tensor where each row contains (num_samples) **indices** sampled from 
+        # multinomial probability distribution located in the corresponding row of tensor input.
+        noise_words = torch.multinomial(input       = noise_dist,           # input tensor containing probabilities
+                                        num_samples = batch_size*n_samples, # number of samples to draw
+                                        replacement = True)
         noise_words = noise_words.to(device)
         
-        embedded_noise_words = self.output_embed(noise_words).view(batch_size,
-                                                                   n_samples,
-                                                                   self.n_embed)
+        # use context matrix for embedding noise samples
+        noise_vectors = self.out_embed(noise_words).view(batch_size, n_samples, self.n_embed)
         
-        return embedded_noise_words
+        return noise_vectors
 
 
 ###############################################################################
 #########################  9. DEFINE LOSS FUNCTION  ###########################
 ###############################################################################
 class SkipGram_NegSample_Loss(nn.Module):
+  def __init__(self):
+    super().__init__()
+
+  def forward(self, 
+              input_vectors, 
+              output_vectors, 
+              noise_vectors):
+      
+    batch_size, embed_size = input_vectors.shape
     
-    def __init__(self):
-        super().__init__()
-        
+    input_vectors = input_vectors.view(batch_size, embed_size, 1)   # batch of column vectors
+    output_vectors = output_vectors.view(batch_size, 1, embed_size) # batch of row vectors
     
-    def forward (self, embedded_input_words, embedded_target_words, embedded_noise_words):
-        
-        n_batches, n_embed = embedded_input_words.shape
-        
-        device = 'cuda' if torch.cuda.is_available else 'cpu'
-        embedded_input_words = torch.LongTensor(embedded_input_words).to(device)
-        # make embedded_input_words a batch of Column Vectors
-        embedded_input_words = embedded_input_words.view(n_batches, n_embed, 1)  
-        
-        embedded_target_words = torch.LongTensor(embedded_target_words).to(device)
-        # make embedded_target_words a batch of Row Vectors
-        embedded_target_words = embedded_target_words.view(n_batches, 1, n_embed)  
-        
-        ## Define Loss Components
-        target_loss = torch.bmm(embedded_target_words, embedded_input_words).sigmoid().log()
-        target_loss = target_loss.squeeze()
-        
-        noise_loss = torch.bmm(embedded_noise_words.neg(), embedded_input_words).sigmoid().log()
-        noise_loss = noise_loss.squeeze().sum(dim=1)
+    # correct log-sigmoid loss
+    out_loss = torch.bmm(output_vectors, input_vectors).sigmoid().log().squeeze()
     
-        return -(target_loss + noise_loss).mean()  # return average batch loss
+    # incorrect log-sigmoid loss
+    noise_loss = torch.bmm(noise_vectors.neg(), input_vectors).sigmoid().log()
+    noise_loss = noise_loss.squeeze().sum(1)  # sum the losses over the sample of noise vectors
+
+    return -(out_loss + noise_loss).mean()  # average batch loss
 
 
 ###############################################################################
@@ -184,7 +189,7 @@ from torch import optim
 embedding_dim = 300
 model = SkipGram_NegSample_Model( len(vocab_to_int), embedding_dim, noise_dist )
 criterion = SkipGram_NegSample_Loss()
-optimizer = optim.Adam(model.parameters(), lr = 0.003)
+optimizer = optim.Adam(model.parameters(), lr=0.003)
 
 
 ###############################################################################
@@ -195,32 +200,52 @@ step = 0
 n_epochs = 5
 device = 'cuda' if torch.cuda.is_available else 'cpu'
 
-for epoch in range(n_epochs):
-    for inputs, targets in get_batches(int_words, batch_size = 512):
-        step += 1
-        inputs, targets = torch.LongTensor(inputs).to(device), torch.LongTensor(targets).to(device)
-        
-        embedded_input_words = model.forward_input(inputs)
-        embedded_target_words = model.forward_target(targets)
-        embedded_noise_words = model.forward_noise(inputs.shape[0], n_samples = 5)
-
-        loss = criterion(embedded_input_words, embedded_target_words, embedded_noise_words)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        if (step % print_every) == 0:
-            print("Epoch: {}/{}".format((epoch+1), n_epochs))
-            print("Loss: {:.4f}".format(loss.item()))
-            valid_idxs, similarities = cosine_similarity(model.embed, device)
-            _, closest_idxs = similarities.topk(6)
-            valid_idxs, closest_idxs = valid_idxs.to(device), closest_idxs.to(device)
+def train_skipgram(model,
+                   criterion,
+                   optimizer,
+                   int_words,
+                   n_negative_samples=5,
+                   batch_size=512,
+                   n_epochs=5,
+                   print_every=1500,
+                   ):
+    model.to(device)
+    
+    step = 0
+    for epoch in range(n_epochs):
+        for inputs, targets in get_batches(int_words, batch_size=batch_size):
+            step += 1
+            inputs = torch.LongTensor(inputs).to(device)    # [b*n_context_words]
+            targets = torch.LongTensor(targets).to(device)  # [b*n_context_words]
             
-            for ii, v_idx in enumerate(valid_idxs):
-                closest_words = [int_to_vocab[idx] for idx in closest_idxs[ii]][1:]
-                print(int_to_vocab[v_idx] + " | "+ ", ".join(closest_words))
-                
-            print("...")
+            embedded_input_words = model.forward_input(inputs)
+            embedded_target_words = model.forward_target(targets)
+            embedded_noise_words = model.forward_noise(batch_size=inputs.shape[0], 
+                                                      n_samples=n_negative_samples)
+
+            loss = criterion(embedded_input_words, embedded_target_words, embedded_noise_words)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            if (step % print_every) == 0:
+              print("Epoch: {}/{}".format((epoch+1), n_epochs))
+              print("Loss: {:.4f}".format(loss.item()))
+              valid_idxs, similarities = cosine_similarity(model.in_embed)
+              _, closest_idxs = similarities.topk(6)
+              valid_idxs, closest_idxs = valid_idxs.to('cpu'), closest_idxs.to('cpu')
+              
+              for ii, v_idx in enumerate(valid_idxs):
+                closest_words = [int_to_vocab[idx.item()] for idx in closest_idxs[ii]][1:]
+                print(int_to_vocab[v_idx.item()] + " | "+ ", ".join(closest_words))
+
+              print("...")
+            
+ train_skipgram(model,
+               criterion,
+               optimizer,
+               int_words,
+               n_negative_samples=5)
 
 
 ###############################################################################
